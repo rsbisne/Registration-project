a2a09bc99326cece280b8466b5be2e72caf53144
rsbisne <roshansb99@gmail.com>
2023-07-27 14:00:23 +0530

    deployed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import Signup from "./pages/Signup";
 import Forgot from "./pages/Forgot";
 import Switch from "./components/Switch";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
+
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <div className="App">
-      <Switch />
-      <Router>
+    <QueryClientProvider client={queryClient}>
+      <HashRouter>
+        <Switch />
         <Routes>
           <Route path="/" element={<SignIn />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/home" element={<Home />} />
           <Route path="/forgot" element={<Forgot />} />
         </Routes>
-      </Router>
-    </div>
+      </HashRouter>
+    </QueryClientProvider>
   );
 }
 
